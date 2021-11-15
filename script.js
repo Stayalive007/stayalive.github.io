cacff0e2a5aa693c806299755f4c1f7a75171879
Stayalive007 <94207460+Stayalive007@users.noreply.github.com>
2021-11-15 05:30:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addOg() {
+    var og = document.querySelector("link");
+    og.href = "style.css";
+}
+
+function addBlue() {
+    var cf = document.querySelector("link");
+    cf.href = "blue.css";
+}
+
+function addGreen() {
+    var el = document.querySelector("link");
+    el.href = "green.css";
+}
+
+function addPurple() {
+    var pl = document.querySelector("link");
+    pl.href = "purple.css";
+}
